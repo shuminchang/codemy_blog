93a5f8504f8871a51954009d6b4dc1f076484742
shuminchang <magic2313@gmail.com>
2023-10-26 11:06:22 +0800

    disable Permissions at Edit Setting Page

--- members/forms.py
+++ members/forms.py
@@ -42,9 +42,9 @@ class EditProfileForm(UserChangeForm):
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-checkt'}))
+    is_superuser = forms.CharField(max_length=100, disabled=True, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_staff = forms.CharField(max_length=100, disabled=True, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_active = forms.CharField(max_length=100, disabled=True, widget=forms.CheckboxInput(attrs={'class': 'form-checkt'}))
     date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
