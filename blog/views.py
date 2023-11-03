3c3e7cf0663bb339996063fdbe44c02deaea1dea
shuminchang <magic2313@gmail.com>
2023-11-03 11:28:35 +0800

    paginate home page

--- blog/views.py
+++ blog/views.py
@@ -24,6 +24,7 @@ class HomeView(ListView):
     template_name = 'home.html'
     # ordering = ['-id']   order by id in descending order
     ordering = ['-post_date']
+    paginate_by = 5
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
