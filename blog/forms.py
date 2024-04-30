c813e233ebe2aed85c88319422a346ffbfd6cdff
shuminchang <magic2313@gmail.com>
2024-04-30 08:45:03 +0800

    modify PostForm for quering after form initialized

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from .models import Post, Category, Comment
 
-# choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment')]
-choices = Category.objects.all().values_list('name', 'name')
-choice_list = []
-
-for item in choices:
-    choice_list.append(item)
-
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        choices = Category.objects.all().values_list('name', 'name')
+        choice_list = []
+        for item in choices:
+            choice_list.append(item)
+        self.fields['category'].choices = choice_list
+
     class Meta:
         model = Post
         fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
@@ -16,9 +17,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
         }
