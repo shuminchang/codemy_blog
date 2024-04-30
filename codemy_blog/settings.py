84fcd3458f32222e89c2b73937289f2dd36d8663
shuminchang <magic2313@gmail.com>
2024-04-30 08:53:08 +0800

    migrate to postgresql

--- codemy_blog/settings.py
+++ codemy_blog/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'codemy_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codemy_blog',
+        'USER': 'shumin',
+        'PASSWORD': 'shumin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
