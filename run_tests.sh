bdac06e75d17cd2304c67d440ac4db436e976e12
Shumin Chang <46363600+shuminchang@users.noreply.github.com>
2024-05-06 17:03:04 +0800

    Create run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Activate virtual environment
+# Change the path as needed to where your virtual environment is located
+source /home/jenkins/envs/myenv/bin/activate
+
+# Change directory to where your Django project is
+# This should be where your manage.py file is located
+cd /var/lib/jenkins/workspace/Shumin Blog
+
+# Run Django tests
+python manage.py test
