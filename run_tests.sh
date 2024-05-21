4ee61565d9302fb864a822996d2873abc3d20a94
shuminchang <magic2313@gmail.com>
2024-05-21 22:52:01 +0800

    update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 source /var/lib/jenkins/envs/venv/bin/activate
 
 # Change directory to where your Django project is
-cd "/var/lib/jenkins/workspace/Shumin Blog"
+cd "/var/lib/jenkins/workspace/Shumin Blog dev"
 
 # Run Django tests using the full path to the Python executable
 /var/lib/jenkins/envs/venv/bin/python manage.py test
