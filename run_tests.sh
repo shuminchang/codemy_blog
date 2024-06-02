f4a6f8af8ffca5bc27cc2f2dfcf87514aae26b02
shuminchang <magic2313@gmail.com>
2024-06-02 20:15:52 +0800

    update run_tests.sh for installing dependencies

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,8 @@ source /var/lib/jenkins/envs/venv/bin/activate
 # Change directory to where your Django project is
 cd "/var/lib/jenkins/workspace/Shumin Blog dev"
 
+# Install dependencies
+/var/lib/jenkins/envs/venv/bin/pip install -r requirements.txt
+
 # Run Django tests using the full path to the Python executable
 /var/lib/jenkins/envs/venv/bin/python manage.py test
