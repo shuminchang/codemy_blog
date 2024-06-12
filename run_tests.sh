75427e197732ee4e6f6210c087abf47293388a7f
shuminchang <magic2313@gmail.com>
2024-06-12 17:03:15 +0800

    Improve run_tests.sh to start the server for image orientation tests

--- run_tests.sh
+++ run_tests.sh
@@ -11,5 +11,17 @@ cd "/var/lib/jenkins/workspace/Shumin Blog dev"
 # Install dependencies
 /var/lib/jenkins/envs/venv/bin/pip install -r requirements.txt
 
+# Start the Django development server in the background
+/var/lib/jenkins/envs/venv/bin/python manage.py runserver &
+
+# Store the server's PID to terminate it later
+SERVER_PID=$!
+
+# Give the server a few seconds to start up
+sleep 5
+
 # Run Django tests using the full path to the Python executable
 /var/lib/jenkins/envs/venv/bin/python manage.py test
+
+# Terminate the Django development server
+kill $SERVER_PID
